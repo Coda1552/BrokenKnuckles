9a4acf937e83de58ab154fdec1045ff6a211b5ac
Coda <cody.koelliker@outlook.com>
2023-08-01 21:41:31 -0600

    fix what i deleted

---
+++ src/main/java/codyhuh/brokenknuckles/common/items/DwarvenArmorItem.java
@@ -0,0 +1,12 @@
+package codyhuh.brokenknuckles.common.items;
+
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+
+public class DwarvenArmorItem extends ArmorItem {
+
+    public DwarvenArmorItem(ArmorMaterial pMaterial, Type pType, Properties pProperties) {
+        super(pMaterial, pType, pProperties);
+    }
+
+}
