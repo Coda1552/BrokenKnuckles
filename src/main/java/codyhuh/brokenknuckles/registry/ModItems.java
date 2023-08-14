5b406e79e7d480732723d16ec5e92b5e47ac46f1
DENOBODY2 <amschildkraut@gmail.com>
2023-08-13 21:37:40 -0500

    -Removed old armor item

--- src/main/java/codyhuh/brokenknuckles/registry/ModItems.java
+++ src/main/java/codyhuh/brokenknuckles/registry/ModItems.java
@@ -32,16 +32,15 @@ public class ModItems {
     public static final RegistryObject<Item> DWARVEN_STEEL_AXE = ITEMS.register("dwarven_steel_axe", () -> new DwarvenAxeItem(ModToolTiers.DWARVEN_STEEL, 3, -3, new Item.Properties().fireResistant()));
     public static final RegistryObject<Item> DWARVEN_STEEL_HAMMER = ITEMS.register("dwarven_steel_hammer", () -> new DwarvenHammerItem(5, -3.5F, ModToolTiers.DWARVEN_STEEL_HAMMER, new Item.Properties().fireResistant()));
 
-    public static final RegistryObject<Item> MAGIC_CROWN = ITEMS.register("magic_crown", () -> new CultArmorItem(ModArmorMaterials.INVIS, ArmorItem.Type.HELMET, new Item.Properties()));
-    public static final RegistryObject<Item> MAGIC_CLOAK = ITEMS.register("magic_cloak", () -> new CultArmorItem(ModArmorMaterials.INVIS, ArmorItem.Type.CHESTPLATE, new Item.Properties()));
-    public static final RegistryObject<Item> MAGIC_PANTS = ITEMS.register("magic_pants", () -> new CultArmorItem(ModArmorMaterials.INVIS, ArmorItem.Type.LEGGINGS, new Item.Properties()));
-    public static final RegistryObject<Item> MAGIC_BOOTS = ITEMS.register("magic_boots", () -> new CultArmorItem(ModArmorMaterials.INVIS, ArmorItem.Type.BOOTS, new Item.Properties()));
-
     public static final RegistryObject<Item> OCEAN_HELMET = ITEMS.register("ocean_helmet", () -> new OceanArmorItem(ModArmorMaterials.OCEAN, ArmorItem.Type.HELMET, new Item.Properties()));
     public static final RegistryObject<Item> OCEAN_CHESTPLATE = ITEMS.register("ocean_chestplate", () -> new OceanArmorItem(ModArmorMaterials.OCEAN, ArmorItem.Type.CHESTPLATE, new Item.Properties()));
     public static final RegistryObject<Item> OCEAN_LEGGINGS = ITEMS.register("ocean_leggings", () -> new OceanArmorItem(ModArmorMaterials.OCEAN, ArmorItem.Type.LEGGINGS, new Item.Properties()));
     public static final RegistryObject<Item> OCEAN_BOOTS = ITEMS.register("ocean_boots", () -> new OceanArmorItem(ModArmorMaterials.OCEAN, ArmorItem.Type.BOOTS, new Item.Properties()));
 
+    public static final RegistryObject<Item> MAGIC_CROWN = ITEMS.register("magic_crown", () -> new CultArmorItem(ModArmorMaterials.INVIS, ArmorItem.Type.HELMET, new Item.Properties()));
+    public static final RegistryObject<Item> MAGIC_CLOAK = ITEMS.register("magic_cloak", () -> new CultArmorItem(ModArmorMaterials.INVIS, ArmorItem.Type.CHESTPLATE, new Item.Properties()));
+    public static final RegistryObject<Item> MAGIC_PANTS = ITEMS.register("magic_pants", () -> new CultArmorItem(ModArmorMaterials.INVIS, ArmorItem.Type.LEGGINGS, new Item.Properties()));
+    public static final RegistryObject<Item> MAGIC_BOOTS = ITEMS.register("magic_boots", () -> new CultArmorItem(ModArmorMaterials.INVIS, ArmorItem.Type.BOOTS, new Item.Properties()));
     public static final RegistryObject<Item> SHADOW_CONTROLLER = ITEMS.register("shadow_controller_permanent", () -> new ShadowControllerItem(new Item.Properties().fireResistant()));
     public static final RegistryObject<Item> TEMP_SHADOW_CONTROLLER = ITEMS.register("shadow_controller_temp", () -> new TempShadowControllerItem(new Item.Properties().fireResistant()));
     public static final RegistryObject<Item> BARRIER_SETTINGS_WAND = ITEMS.register("barrier_settings_wand", () -> new SettingsWandItem(new Item.Properties().fireResistant()));
